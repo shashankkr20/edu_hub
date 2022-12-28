06ab9ea5bad94e5cf7445ac0b8729f73e10e525a
pandiji <skm808478@gmail.com>
2022-12-28 16:56:17 +0530

    first

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let option=document.querySelector(".option")
 let cross=document.querySelector(".cross")
 let b=document.querySelector(".sidebarMenu")
+let act_name=document.querySelector(".act-name")
+let act_img=document.querySelector(".act-img")
+let act_desc=document.querySelector(".act-desc")
+let radio=[]
+let currentPerson=0;
+radio=document.querySelectorAll(".radio-btn")
 option.onclick = function(){opt_func()};
 function opt_func()
 {
@@ -14,4 +20,44 @@ function cross_func()
     b.classList.add("slideright");
     console.log("slided right")
     b.classList.remove("slideleft");
+}
+const person = [
+    {
+        "image": "./images/plant.jpg",
+        "Name": "Plantation drive 2K22",
+        "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora libero minus, veritatis consequatur ex enim officia in sequi quidem culpa sapiente temporibus commodi deserunt soluta perferendis ut rem quos iure!",
+    },
+    {
+        "image": "./images/sport.webp",
+        "Name": "Sports day 2K22", 
+        "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora libero minus, veritatis consequatur ex enim officia in sequi quidem culpa sapiente temporibus",
+    },
+    {
+        "image": "./images/cult.jpg",
+        "Name": "Cultora(Cultural-fest) 2K22",
+        "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. enim officia in sequi quidem culpa sapiente temporibus",
+    },
+    {
+        "image": "./images/sci.jpg",
+        "Name": "Science exhibition 2K22",
+        "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora libero minus, veritatis consequatur ex enim officia in sequi quidem culpa sapiente temporibus commodi deserunt soluta perferendis ut rem quos iure! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, laboriosam?",
+    }
+];
+for(let i = 0; i < radio.length; i++) {
+    radio[i].addEventListener("click", function() {
+        updateevent(i);
+    });
+}
+function updateevent(presentPerson) {
+    act_img.setAttribute("src", person[presentPerson].image);
+    act_name.innerHTML = person[presentPerson].Name;
+    act_desc.innerHTML = person[presentPerson].desc;
+
+    // changing radio buttons
+    for(let i = 0; i < person.length; i++) {
+        radio[i].checked=false;
+    }
+    radio[presentPerson].checked=true;
+
+    currentPerson = presentPerson;
 }
